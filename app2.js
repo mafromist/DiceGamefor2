a18185a55a5ee64a21806ed740947fc1451b2c55
MugeMeggy <mafromist@gmail.com>
2021-02-06 02:25:40 +0000

    #DiceGame Still there are bugs
    but the game is a bit working now

--- app2.js
+++ app2.js
@@ -1,53 +1,48 @@
 let player1Score = 0;
 let player2Score = 0;
 
+let hold1Score = 0;
+let hold2Score = 0;
+
 let currentPlayer;
 
 const pickAPlayer = () => Math.round(Math.random() + 1);
 
 const rollDice = () => Math.round((Math.random() * 5) + 1);
 
-currentPlayer = pickAPlayer();
-console.log(currentPlayer);
-
-currentDice = rollDice();
-console.log(currentDice);
 
 const dots = document.querySelectorAll(".dot");
 const dice = document.querySelector(".dice");
 
-const diceStyling = (num) => {
-    dice.style.display = "flex";
-    for (let dot of dots){
-        dot.style.display = "none";
-    }
-    for(let i = 0; i <= num - 1; i++) {
-        dots[i].style.display = "block"
-    }
-}
-
 const p1Bg = document.querySelector(".playerOne");
 const p2Bg = document.querySelector(".playerTwo");
-
-const  switchBg = () => { 
-    if (currentPlayer === 1) {
-        p1Bg.style.backgroundColor = "white";
-        p2Bg.style.backgroundColor = "transparent";
-    } else {
-        p1Bg.style.backgroundColor = "transparent";
-        p2Bg.style.backgroundColor = "white";
-    }
-}
     
 const rollBtnStyle = document.querySelector(".rollBtn");
 const holdBtnStyle = document.querySelector(".holdBtn");
 const newGameBtn = document.querySelector("#newGameBtn");
 
+const currentScore1 = document.querySelector("#currentScore1");
+const currentScore2 = document.querySelector("#currentScore2");
+const holdScore1 = document.querySelector("#holdScore1");
+const holdScore2 = document.querySelector("#holdScore2");
+
+const rollBtn = document.querySelector("#rollBtn");
+const holdBtn = document.querySelector("#holdBtn");
+
+
 const newGame = () => {
-    console.log("newGame");
-    
+    pickAPlayer();
+    currentPlayer = pickAPlayer();
+
     player1Score = 0;
     player2Score = 0;
+    hold1Score = 0;
+    hold2Score = 0;
+
+    currentScore1.textContent = 0;
+    currentScore2.textContent = 0;
+    holdScore1.textContent = 0;
+    holdScore2.textContent = 0;
 
     diceStyling(1);
     switchBg();
@@ -60,79 +55,99 @@ newGameBtn.addEventListener("click", () => {
     newGame();
 });
 
-const currentScore1 = document.querySelector("#currentScore1");
-const currentScore2 = document.querySelector("#currentScore2");
 
-const addScore = (num) => {
-    if(currentPlayer === 1) {
-        player1Score = num + holdScore1.textContent;
-        console.log(player1Score);
-        currentScore1.textContent = player1Score;
-    } else {
-        currentScore2.textContent = num + holdScore2.textContent;
-    }
-    console.log(player1Score);
-    console.log(player2Score);
-}
-    
-const game = (num) => {
-    if(player1Score && player2Score < 20) {
-        addScore(num);
-        console.log(player1Score);
-        console.log(player2Score);
-    } else if (player1Score >= 20) {
-        currentScore1.textContent = "You Win!";
-    } else if (player2Score >= 20) {
-        currentScore2.textContent = "You Win!";
-    }
-    
-}
-
-const rollBtn = document.querySelector("#rollBtn");
+//holdScore un ustune ekleme yapamiyor
 
 rollBtn.addEventListener("click", () => {
     rollDice();
     currentDice = rollDice();
     diceStyling(currentDice);
     console.log(currentDice);
-    game(currentDice);
-});
-
-const holdScore1 = document.querySelector("#holdScore1");
-const holdScore2 = document.querySelector("#holdScore2");
-
-const holdFunc = () => {
-    //ternary operator if/else as below
-    currentPlayer = (currentPlayer === 1) ? 2 : 1;
-    // everything on the left of there is the test
-    // left is the true branch
-    // right side is after colon is False branch
-    switchBg();
-
-    if(currentPlayer === 1){
-        holdScore1.textContent = player1Score;
-        currentScore1.textContent = 0;
-   
-    } else {
-        holdScore2.textContent = player2Score;
-        currentScore2.textContent = 0;
+    
+    if (currentDice !== 1 && hold1Score === 0) {    
+        if (currentPlayer == 1) {
+            player1Score = player1Score + currentDice;
+            currentScore1.textContent = player1Score;
+        } else {
+            player2Score = player2Score + currentDice;
+            currentScore2.textContent = player2Score;
+        }
+    } else if (currentDice !== 1 && hold1Score !== 0) {    
+        if (currentPlayer == 1) {
+            let afterHoldScore1 = 0;
+            player1Score = hold1Score + currentDice ;
+            currentScore1.textContent = player1Score;
+        } else {
+            player2Score = hold2Score + currentDice;
+            currentScore2.textContent = player2Score;
+        }
+    } else if (currentDice === 1) {
+        currentScore1.textContent = (currentPlayer === 1) ? "You Lost" : "You Win!";
+        currentScore2.textContent = (currentPlayer === 2) ? "You Lost" : "You Win!";
     }
-
-    console.log(currentPlayer);
-    console.log(player1Score);
-    console.log(player2Score);
-    console.log("holdScore");
-}
-
-const holdBtn = document.querySelector("#holdBtn");
+    doesWin();
+});
 
 holdBtn.addEventListener("click", () => {
     holdFunc();
-    switchBg();
 });
 
 
+//holdScore kendisine eklenmesi gerekiyor
+
+const holdFunc = () => {
+    if(currentPlayer === 1){
+        hold1Score = player1Score;
+        holdScore1.textContent = player1Score;
+        currentScore1.textContent = 0;
+        p1Bg.style.backgroundColor = "transparent";
+        p2Bg.style.backgroundColor = "white";
+    } else {
+        hold2Score = player2Score;
+        holdScore2.textContent = player2Score;
+        currentScore2.textContent = 0;
+        p1Bg.style.backgroundColor = "transparent";
+        p2Bg.style.backgroundColor = "white";
+    }
+    currentPlayer = (currentPlayer === 1) ? 2 : 1;
+    switchBg();
+}
+
+const doesWin = () => {
+    if (player1Score >= 20) {
+        currentScore1.textContent = "You Win!";
+        currentScore2.textContent = "You Lost!";
+    } else if (player2Score >= 20) {
+        currentScore1.textContent = "You Lost!";
+        currentScore2.textContent = "You Win!";    
+    }
+}
+
+const  switchBg = () => { 
+    if (currentPlayer === 1) {
+        p1Bg.style.backgroundColor = "white";
+        p2Bg.style.backgroundColor = "transparent";
+    } else {
+        p1Bg.style.backgroundColor = "transparent";
+        p2Bg.style.backgroundColor = "white";
+    }
+}
+
+const diceStyling = (num) => {
+    dice.style.display = "flex";
+    for (let dot of dots){
+        dot.style.display = "none";
+    }
+    for(let i = 0; i <= num - 1; i++) {
+        dots[i].style.display = "block"
+    }
+}
 
 
 
 
+//ternary operator if/else as below
+//currentPlayer = (currentPlayer === 1) ? 2 : 1;
+// everything on the left of there is the test
+// left is the true branch
+// right side is after colon is False branch
